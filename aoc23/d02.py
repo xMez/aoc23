5e1986cf72dc9ba7a075f26daa40175249b96ce6
Mez <5909610+xMez@users.noreply.github.com>
2023-12-21 04:21:38 +0100

    Solve day 1 part 2

--- aoc23/d02.py
+++ aoc23/d02.py
@@ -1,4 +1,5 @@
 import logging
+import math
 from typing import TextIO
 
 import regex as re
@@ -7,7 +8,7 @@ limits = {"r": 12, "g": 13, "b": 14}
 pattern = r"\d+ [r|g|b]"
 
 
-def solve(file: TextIO) -> int:
+def solve_p1(file: TextIO) -> int:
     sum = 0
     for line in file:
         game_str, cube_str = line.split(":")
@@ -25,3 +26,22 @@ def solve(file: TextIO) -> int:
         except StopIteration:
             pass
     return sum
+
+
+def solve_p2(file: TextIO) -> int:
+    sum = 0
+    for line in file:
+        _, cube_str = line.split(":")
+        rounds = cube_str.strip().split(";")
+        count = {"r": 0, "g": 0, "b": 0}
+        for round in rounds:
+            cubes: list[str] = re.findall(pattern, round)
+            for cube in cubes:
+                v, k = cube.split(" ")
+                count[k] = int(v) if int(v) > count[k] else count[k]
+        sum += math.prod(count.values())
+    return sum
+
+
+def solve(file: TextIO) -> int:
+    return solve_p2(file)
--- tests/test_d02.py
+++ tests/test_d02.py
@@ -1,6 +1,6 @@
 import io
 
-from aoc23.d02 import solve
+from aoc23.d02 import solve_p1, solve_p2
 
 
 def test_solve_p1():
@@ -11,4 +11,15 @@ Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
     """.strip()
-    assert solve(io.StringIO(input)) == 8
+    assert solve_p1(io.StringIO(input)) == 8
+
+
+def test_solve_p2():
+    input = """
+Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+    """.strip()
+    assert solve_p2(io.StringIO(input)) == 2286
